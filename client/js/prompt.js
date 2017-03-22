2366096216227552a6fe5f0d071fb7a3d5b6af44
kyleaedwards <edwards.kyle.a@gmail.com>
2017-03-21 23:14:58 -0500

    add sticky command prompts

--- client/js/prompt.js
+++ client/js/prompt.js
@@ -1,4 +1,26 @@
 /**
+ * Constants
+ */
+const DEFAULT_PROMPT = '> ';
+const MODIFIER_KEYS = {
+  16: 'shift',
+  17: 'control',
+  18: 'option',
+  91: 'leftCommand', // Gotta figure out good names
+  93: 'rightCommand', // Because of the Mac/PC discrepancies
+};
+const ACTION_KEYS = {
+  8: 'backspace',
+  13: 'return',
+  27: 'esc',
+  188: 'comma',
+  190: 'period',
+  192: 'backtick',
+  220: 'backslash',
+  222: 'apostrophe',
+};
+
+/**
  * MUD prompt element to connect to both the display and socket.
  */
 module.exports = class Prompt {
@@ -21,8 +43,10 @@ module.exports = class Prompt {
     // Create the pre-prompt text.
     this.pre = document.createElement('span');
     this.pre.id = 'pre';
-    this.pre.innerText = '> ';
+    this.pre.innerText = DEFAULT_PROMPT;
     this.preStack = [];
+
+    this.heldKeys = {};
 
     // Create the container.
     this.gutter = document.createElement('div');
@@ -31,12 +55,16 @@ module.exports = class Prompt {
     this.gutter.appendChild(this._);
     this.app._app.appendChild(this.gutter);
 
+    this.context = false;
+
     // Initialize prompt concealment for passwords.
     this.concealed = false;
 
-    // Capture [RETURN] key.
-    this._handleKeypress = this.handleKeypress.bind(this);
-    this._.addEventListener('keyup', this._handleKeypress);
+    // Capture keypresses.
+    this._handleKeyDown = this.handleKeyDown.bind(this);
+    this._handleKeyUp = this.handleKeyUp.bind(this);
+    this._.addEventListener('keyup', this._handleKeyUp);
+    this._.addEventListener('keydown', this._handleKeyDown);
 
     // Keep focus on prompt element.
     this._refocus = this.refocus.bind(this);
@@ -66,9 +94,11 @@ module.exports = class Prompt {
    */
   clear() {
     this._.value = '';
-    this.pre.innerText = (this.preStack.pop() || '> ');
-    this._.type = 'text';
-    this.concealed = false;
+    if (!this.context) {
+      this.pre.innerText = (this.preStack.pop() || DEFAULT_PROMPT);
+      this._.type = 'text';
+      this.concealed = false;
+    }
   }
 
   /**
@@ -77,6 +107,24 @@ module.exports = class Prompt {
   setPre(pre) {
     this.preStack.push(this.pre.innerText);
     this.pre.innerText = pre + ' ';
+  }
+
+  /**
+   * Sets a command context, in which all subsequent prompts will
+   * apply as arguments to the command until an escape key is pressed.
+   */
+  setContext(context) {
+    this.context = context;
+    this.context.previousPrompt = this.pre.innerText;
+    this.pre.innerHTML = `<span class="command">${DEFAULT_PROMPT}${context.command} </span>`;
+  }
+
+  /**
+   * Clears the command context.
+   */
+  clearContext(context) {
+    this.context = null;
+    this.clear();
   }
 
   /**
@@ -96,6 +144,10 @@ module.exports = class Prompt {
   submit(value) {
     value = value.trim();
     let lastLine = `${this.pre.innerText} `;
+    if (this.context) {
+      lastLine = this.context.previousPrompt;
+      value = `${this.context.command} ${value}`;
+    }
     if (this.concealed) {
       lastLine += '•'.repeat(value.length);
     } else {
@@ -109,7 +161,8 @@ module.exports = class Prompt {
    * Deactivates the prompt and frees up DOM elements and listeners.
    */
   deactivate() {
-    this._.removeEventListener('keyup', this._handleKeypress);
+    this._.removeEventListener('keydown', this._handleKeyDown);
+    this._.removeEventListener('keyup', this._handleKeyUp);
     this._.removeEventListener('blur', this._refocus);
     this._ = null;
     this.gutter = null;
@@ -118,14 +171,62 @@ module.exports = class Prompt {
   }
 
   /**
-   * Event handler to bind keypresses.
+   * Event handler to bind key up.
    */
-  handleKeypress(e) {
+  handleKeyUp(e) {
     const key = e.which || e.keyCode || 0;
-    if (key === 13) {
+    const action = ACTION_KEYS[key];
+    const modifier = MODIFIER_KEYS[key];
+    let preventDefault = false;
+    switch (action) {
+      case 'return':
+        preventDefault = true;
+        this.submit(this._.value);
+        this.clear();
+        break;
+      case 'apostrophe':
+        if (!this.context && this._.value.length === 1) {
+          this.setContext({
+            command: this.heldKeys.shift ? 'shout' : 'say',
+          });
+          preventDefault = true;
+          this._.value = '';
+        }
+        break;
+      case 'backspace':
+        if (this.context && this._.value.length === 0) {
+          preventDefault = true;
+          this.clearContext();
+        }
+        break;
+      case 'esc':
+        if (this.context) this.clearContext();
+        preventDefault = true;
+        break;
+    }
+    if (modifier) {
+      preventDefault = true;
+      this.heldKeys[modifier] = false;
+    }
+    if (preventDefault) {
       e.preventDefault();
-      this.submit(this._.value);
-      this.clear();
+    }
+  }
+
+  /**
+   * Event handler to bind key up.
+   */
+  handleKeyDown(e) {
+    const key = e.which || e.keyCode || 0;
+    const modifier = MODIFIER_KEYS[key];
+    if (modifier) {
+      e.preventDefault();
+      this.heldKeys[modifier] = true;
+    }
+    if (this.context) {
+      if ((modifier === 'backspace' && this._.value.length === 0) || modifier === 'esc') {
+        this.clearContext();
+      }
     }
   }
 
